14d3045204fe3fe6678c316f5173394b107dd3f4
Pham Van Thanh <88926531+thanhlucifer@users.noreply.github.com>
2023-03-23 20:49:57 +0700

    Add project files.

---
+++ BigSchool_PhamVanThanh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool_PhamVanThanh.MvcApplication" Language="C#" %>
---
+++ BigSchool_PhamVanThanh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_PhamVanThanh.Startup))]
+namespace BigSchool_PhamVanThanh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
